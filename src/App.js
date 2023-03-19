8745b1f29cd72015b0044fd7e1962a483f88b97f
ted <theo_drz@live.com>
2023-03-19 17:59:27 -0400

    renamed photos according to option names, and created shuffleArray and selectHandler functions

--- src/App.js
+++ src/App.js
@@ -4,30 +4,63 @@ import MemoryCard from './components/memoryCard.js'
 import './App.css';
 
 function App() {
-  // Our theme is gunna be Metroid Zero Mission Items
-
   const [currentScore, setCurrentScore] = useState(0);
-  const [highScore, setHighScore] = useState(0);
+  const [highScore, setHighScore] = useState(localStorage.getItem('highScore') || 0);
+  const [selected, setSelected] = useState([]);
 
-  const [memoryCard, setMemoryCard] = useState({
-    // The name of an option must directly correlate to it's image file name
-    options: ['Morph Ball', 'Ball Bomb'],
-    used: [],
-  });
+  const options = [
+    'Ball Bomb', 'Charge Beam', 'Gravity Suit', 'High Jump Boots', 'Ice Beam',
+    'Ledge Grab', 'Long Beam', 'Morph Ball', 'Plasma Beam', 'Screw Attack',
+    'Space Jump', 'Speed Booster', 'Varia Suit', 'Wave Beam',
+  ];
 
   function selectHandler(e) {
-    console.log(e.target.getAttribute('value'));
+    if (selected.includes(e.target.getAttribute('value'))) {
+      // if the value already exists in selected var, you lose, reset the game
+      if (currentScore > highScore) {
+        setHighScore(currentScore);
+        localStorage.setItem('highScore', currentScore);
+        // console.log(`LOCALSTORAGE = ${localStorage.getItem('highScore')}`)
+      }
+      setCurrentScore(0);
+      setSelected([]);
+    } else {
+      // if the value isn't already present in selected var, the game continues
+      setCurrentScore(currentScore + 1);
+      setSelected(selected.concat(e.target.getAttribute('value')));
+    }
   }
 
-  // To shuffle an array, use Fisher-Yates algorithm, or Durstenfeld Shuffle algorithm
+  function shuffleArray(arr) {
+    // uses the Fisher-Yates Algorithm to maximize randomness
+    let result = JSON.parse(JSON.stringify(arr));
+    let i = arr.length - 1;
+    while (i > 0) {
+      let randomIndex = Math.floor(Math.random() * (i + 1));
+      [result[i], result[randomIndex]] = [result[randomIndex], result[i]];
+      i--;
+    }
+    return result;
+  }
 
   return (
     <div>
       <h1>HELLO WORLD</h1>
-      <ScoreBoard currentScore={currentScore} highScore={highScore}/>
-      <MemoryCard memoryCard={memoryCard} selectHandler={selectHandler}/>
+      <ScoreBoard
+      currentScore={currentScore}
+      highScore={highScore}
+      />
+      <MemoryCard
+      options={shuffleArray(options)}
+      selectHandler={selectHandler}
+      />
     </div>
   );
 }
+
+// TO-DO LIST
+//    - [ DONE ] Use localStorage to save highScore
+//    - Styling, go with a dark theme, our icons tend to be pretty bright
+//    - Use a flexible grid like in this project: https://github.com/tediferJones/odin-library
 
 export default App;
--- src/components/memoryCard.js
+++ src/components/memoryCard.js
@@ -3,13 +3,15 @@ import { v4 as uuidv4 } from 'uuid';
 function MemoryCard(props) {
   // console.log(props)
 
-  const optionCards = props.memoryCard.options.map(option => {
+  const optionCards = props.options.map(option => {
     return (
-      <div
-      onClick={props.selectHandler} 
+      <div onClick={props.selectHandler} 
       key={uuidv4()} 
       value={option}>
-        <img src={require(`../assets/images/${option}.png`)} alt={option}/>
+        <img src={require(`../assets/images/${option}.png`)}
+        alt={option}
+        value={option}
+        />
         {option}
       </div>
     )
